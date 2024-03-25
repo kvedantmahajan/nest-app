2ad12eb3f172331850b24db57082f8fbddfd49f6
Vedant Mahajan <kushalvmahajan@gmail.com>
2024-03-25 17:58:03 +0530

    Added coffee service to the mix

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -10,33 +10,35 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
   @Get()
   // DISADVANTAGES OF using Native response is-
   // 1. you loose control over NestJs Response handlers which supports interceptors and decorators
   // 2. Also it can become platform dependant. Remember that Nest supports Fastify and others as well
   // 3. Harder to test as Response needs to be mocked as well
-  findAll(@Query() paginationQuery): string {
-    const { limit, offset } = paginationQuery;
-    return `Here are all results for limit: ${limit} and offset: ${offset}`;
+  findAll(@Query() paginationQuery) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
   }
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Here is the #${id} you requested`;
+    return this.coffeeService.findOne(id);
   }
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
   }
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Updated #${id}`;
+    return this.coffeeService.update(id, body);
   }
   @Delete(':id')
   delete(@Param('id') id: string, @Body() body) {
-    return `Deleted #${id}`;
+    return this.coffeeService.remove(id);
   }
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffees';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Mocha Latte',
+      brand: 'Third wave coffee',
+      flavours: ['chocolate', 'vanilla'],
+    },
+  ];
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return this.coffees;
+  }
+  update(id: string, updateCoffeeDto: Partial<Coffee>) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees[coffeeIndex] = {
+        ...this.coffees[coffeeIndex],
+        ...updateCoffeeDto,
+      };
+      return this.coffees[coffeeIndex];
+    }
+    return [];
+  }
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+    return this.coffees;
+  }
+}
